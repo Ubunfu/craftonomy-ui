32bf5fb9e1b772a3522492b40af8926bfce3d2fd
Ryan Allen <rallen3882@gmail.com>
2022-07-30 13:14:41 -0400

    Adding a Vuex state system to provide a global hook into current window size that should help implement responsive components

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from "@/router";
 import VueCookies from 'vue-cookies'
+import {createStore} from 'vuex'
+
+const store = createStore({
+    state() {
+        return {
+            windowWidth: window.innerWidth,
+        }
+    },
+    mutations: {
+        setWindowWidth(state) {
+            state.windowWidth = window.innerWidth;
+        }
+    }
+})
 
 createApp(App)
+    .use(store)
     .use(router)
     .use(VueCookies)
     .mount('#app')
